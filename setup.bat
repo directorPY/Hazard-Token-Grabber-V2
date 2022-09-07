bdb857366b968fadef5aa68700973cd60d9b97fa
K.Dot <110929758+KDot227@users.noreply.github.com>
2022-09-06 19:55:15 -0500

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,4 +1,29 @@
-python -m pip install -r requirements.txt
+@echo off
+cd /d %~dp0
+python --version 3>NUL
+if errorlevel 1 goto errorNoPython
+pip -v>NUL
+if errorlevel 1 goto errorNoPip
+python -m pip install --upgrade -r requirements.txt
+pip -v>NUL
+if errorlevel 1 goto errorNoPyinstaller
 cls
 start build-exe.bat
-start /b "" cmd /c del "%~f0"&exit /b
+pause
+exit
+
+
+:errorNoPython
+echo Python is not installed on your system or not added to path!!!
+pause
+exit
+
+:errorNoPip
+echo Pip is not installed on your system or not added to path!!!
+pause
+exit
+
+:errorNoPyinstaller
+echo Pyinstaller was not installed correctly meaning it wasn't added to path. Please fix this
+pause
+exit
